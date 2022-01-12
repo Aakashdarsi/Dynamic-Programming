98f33afee38c9c6a4d58ec2c2f85e24d42cd03f5
Aakash Darsi <darsisriguruaakash@gmail.com>
2022-01-12 09:53:16 +0530

    Comments changed in subset sum dp

--- subsetsumdp.py
+++ subsetsumdp.py
@@ -1,9 +1,11 @@
 def subsetSum(set,sume,n):
     dp_table = [[False for i in range(sume+1)] for j in range(n+1)]
     for i in range(n+1):
-        dp_table[i][0] = True  # if the sum =0 and number of elements can be or cannot be zero then we 't find the result
+        dp_table[i][0] = True  # if the sum =0 and number of elements can be or cannot be zero then we can find the result
+    
     for j in range(1,sume+1):
         dp_table[0][j] = False # if number of elements is zero and sum of elements is non zero then we can't find the result
+    
     for i in range(n+1):
         for j in range(sume+1):
             if set[i-1] > j:
@@ -11,11 +13,14 @@ def subsetSum(set,sume,n):
             
             if set[i-1]<=j:
                 dp_table[i][j] = dp_table[i-1][j] or dp_table[i-1][j-set[i-1]]
+    
     for i in range(n+1): # Printing DP Table
         for j in range(sume+1):
             print(dp_table[i][j],end=" | ")
         print()
-    return dp_table[n][sume]
+    
+    
+    return dp_table[n][sume] # Final output
             
 
 
